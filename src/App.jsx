eb8bc363e17a1a41ad5c0fb07af74a636c19e1dc
atif7luqman <atifluqman28@gmail.com>
2023-12-07 18:19:02 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './assets/components/Header'
+import Home from './assets/components/Home'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  )
+}
+
+export default App
