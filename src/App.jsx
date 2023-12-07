5d69162321c56dc564746db83750acb3848809d4
atif7luqman <atifluqman28@gmail.com>
2023-12-07 18:45:48 +0500

    Add Values component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './assets/components/Header'
 import Home from './assets/components/Home'
+import Values from './assets/components/Values'
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Values />
     </div>
   )
 }
