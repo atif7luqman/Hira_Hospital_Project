f49d1b34d7078ca6a71037584cb6505fac95ffe3
atif7luqman <atifluqman28@gmail.com>
2023-12-14 11:06:53 +0500

    footer modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import Home from './assets/components/Home'
 import Values from './assets/components/Values'
 import Doctors from './assets/components/Doctors'
 import Footer from './assets/components/Footer'
+import Booking from './assets/components/Booking'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
     <div>
+      <Routes>
+        
+        {/* <Route path={'/'} element={<><Header /> <Home /> </>} />
+        <Route path={'/values'} element={<Values />} />
+        <Route path={'/docotrs'} element={<Doctors />} />
+        <Route path='/booking' element={<Booking />} /> */}
+        
+      </Routes>
       <Header />
       <Home />
       <Values />
       <Doctors />
+      <Booking />
       <Footer />
     </div>
   )
