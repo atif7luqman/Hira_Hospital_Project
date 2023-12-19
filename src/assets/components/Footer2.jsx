3bbfdf2719232f3a7121eef73a63616b9e3f99d3
atif7luqman <atifluqman28@gmail.com>
2023-12-19 20:29:35 +0500

    Modify update component

--- src/assets/components/Footer2.jsx
+++ src/assets/components/Footer2.jsx
@@ -5,43 +5,41 @@ import { PhoneFilled, EnvironmentFilled, MailFilled } from '@ant-design/icons';
 
 export default function Footer2() {
   return (
-    <Container fluid className="my-5" style={{ background: 'linear-gradient(to right, #4BBDF2, #2359F4)', paddingTop: '4rem' }}>
+    <Container fluid className="my-5 pb-5" style={{ backgroundColor: '#81BAEE', paddingTop: '4rem' }}>
       <hr style={{ borderTop: '5px solid #000000' }} />
       <Row xs={1} md={3} className="g-4">
         {/* First Column */}
-        <Col md={4} className="align-self-center text-center">
-          <div>
-            <Image src={Icon} roundedCircle style={{ width: '6rem', height: '5rem' }} />
-            <p className="text-black mt-2" style={{ fontWeight: 'bold' }}>Hira Hospital</p>
-          </div>
+        <Col md={4} className="d-flex align-items-start">
+            <div className="d-flex align-items-start">
+            <Image src={Icon} />
+            <p className='mt-auto text-end pb-2 fs-3 fw-bold'>Hira Hospital</p>
+            </div>
         </Col>
 
         {/* Second Column */}
-        <Col md={4} className="text-center">
-          <div>
-            <br/><br/><br/>
-            <h5 className="text-black" style={{ fontWeight: 'bold' }}>Hours</h5>
-            <p className="text-black">
-              Monday:       9:00 - 18:00 <br />
-              Tuesday:      9:00 - 18:00 <br />
-              Wednesday:    9:00 - 18:00 <br />
-              Thursday:     9:00 - 18:00 <br />
-              Friday:       9:00 - 18:00
-            </p>
-          </div>
-        </Col>
-
-        {/* Third Column */}
-        <Col md={4} className="text-center">
-          <div>
-          <br/><br/><br/>
-            <h5 className="text-black" style={{ fontWeight: 'bold' }}>Contact</h5>
-            <p className="text-black">
-              <PhoneFilled className='fs-5 px-1' />(071) 5614111 <br />
-              <MailFilled className='fs-5 px-1' />info@hira.com <br />
-              <EnvironmentFilled className='fs-5 px-1' />Sukkur, Pakistan
-            </p>
-          </div>
+        <Col md={8} className="text-center">
+          <Row>
+            <Col className="text-left">
+              <br /><br /><br />
+              <h5 className="text-black" style={{ fontWeight: 'bold' }}>Hours</h5>
+              <p className="text-black">
+                Monday:       9:00 - 18:00 <br />
+                Tuesday:      9:00 - 18:00 <br />
+                Wednesday:    9:00 - 18:00 <br />
+                Thursday:     9:00 - 18:00 <br />
+                Friday:       9:00 - 18:00
+              </p>
+            </Col>
+            <Col className="text-left">
+              <br /><br /><br />
+              <h5 className="text-black" style={{ fontWeight: 'bold' }}>Contact</h5>
+              <p className="text-black">
+                <PhoneFilled className='fs-5 px-1' />(071) 5614111 <br />
+                <MailFilled className='fs-5 px-1' />info@hira.com <br />
+                <EnvironmentFilled className='fs-5 px-1' />Sukkur, Pakistan
+              </p>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
--- src/assets/components/Updates.jsx
+++ src/assets/components/Updates.jsx
@@ -1,28 +1,38 @@
-import Card from 'react-bootstrap/Card';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import HospitalImg from '../images/hospital-image.png'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { HeartFilled, UserOutlined, MedicineBoxFilled } from '@ant-design/icons';
 
 export default function Updates() {
   return (
-    <Container fluid className="text-center my-5" style={{ background: 'linear-gradient(to right, #4BBDF2, #2359F4)', paddingTop: '4rem' }}>
-
-      <Row xs={1} md={6} className="g-4 pb-5">
-        <Col md={6} className="d-flex flex-column align-items-center justify-content-center">
-        <h2 className="fw-bold fs-1 pb-5" style={{color:'#BE2E3E'}}>Daily Hospital Updates</h2>
-
-        <div className='bg-white'>
-  <p className='fs-4 fw-bold text-white' style={{ color: '#081D54', backgroundColor: '#59A0F4' }}>Patients Visited</p>
-</div>
-
-            <div>
-                <br></br>
-            </div>
-            <div className='bg-white'>
-                dif1
-            </div>
+    <Container fluid className="my-5 pb-5" style={{ background: 'linear-gradient(to right, #4BBDF2, #2359F4)', paddingTop: '4rem' }}>
+      <hr style={{ borderTop: '5px solid #000000' }} />
+      <h2 className="fw-bold fs-1 pb-5 text-center" style={{ color: '#BE2E3E' }}>Daily Hospital Updates</h2>
+      <Row xs={1} md={3} className="g-4">
+        {/* First Column */}
+        <Col md={4} className="align-self-center text-center">
+          <div className="d-flex flex-column align-items-center">
+            <HeartFilled className='fs-1 text-danger mb-2' />
+            <span className="fw-bold fs-1 text-dark" style={{ color: '##BE2E3E' }}>99%</span>
+            <span className="fw-bold text-danger">Positive Feedback</span>
+          </div>
         </Col>
-        <Col md={6}>
-            <Image src={HospitalImg} roundedCircle style={{ width: '250px', height: '250px' }} />
+
+        {/* Second Column */}
+        <Col md={4} className="text-center">
+          <div className="d-flex flex-column align-items-center">
+            <UserOutlined className='fs-1 text-danger' />
+            <span className="fw-bold fs-1 text-dark" style={{ color: '#2359F4' }}>2500+</span>
+            <span className="fs-6 text-danger">happy patients a week</span>
+          </div>
+        </Col>
+
+        {/* Third Column */}
+        <Col md={4} className="text-center">
+          <div className="d-flex flex-column align-items-center">
+            <MedicineBoxFilled className='fs-1 text-danger' />
+            <span className="fw-bold fs-1 text-dark" style={{ color: '#2359F4' }}>43</span>
+            <span className="fs-6 text-danger">professional doctors</span>
+          </div>
         </Col>
       </Row>
     </Container>
